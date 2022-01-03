5b4581246943e1d0ec8b0d00156725d83538edfe
sugarmouse <tanghao19950911@gmail.com>
2022-01-03 15:40:46 +0800

    html-webpack-plugin 自动生成HTML文件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,4 @@
  console.log('hi4')
+ const $h2 = document.createElement('h2')
+ $h2.innerText = 'this is h4'
+ document.body.appendChild($h2)
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'production',
   entry: {
     index: './lib/index.tsx' // 定义项目入口 文件名：地址
   },
-  output:{
-    path: path.resolve(__dirname,'dist/lib'),
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
     library: 'forestUI', // 库的名称
     libraryTarget: 'umd', //封装成什么模块定义  commonJS, umd, amd
 
   },
   module: {
     rules: [
-       {
-         test: /\.tsx?$/,
-         loader: 'awesome-typescript-loader'
-       }
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'forestUI',
+      template: 'index.html'
+    })
+  ]
 }
